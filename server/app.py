a80e94ef40a47649cbf6a8a7c2fb8250615c55a5
Metellus509 <metellusjunior56@gmail.com>
2025-10-07 02:55:07 +0000

    complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
+        response = make_response(messages, 200)
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.json.get('body'),
+            username=request.json.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        response = make_response(new_message.to_dict(), 201)
+    return response
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET','DELETE','PATCH'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if message == None :
+        response = make_response({"error": "Message not found"}, 404)
+    else:
+        if request.method == 'GET':
+            response = make_response(message.to_dict(), 200)
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+            response = make_response({}, 204)
+        elif request.method == 'PATCH':
+            for attr in request.json:
+                setattr(message, attr, request.json.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            response = make_response(message.to_dict(), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
